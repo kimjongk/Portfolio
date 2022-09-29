9f839c3928a0c43af577e6ffe688be8e4bed5d51
kimjongk <104364725+kimjongk@users.noreply.github.com>
2022-09-29 22:25:04 +0900

    JS_Navbar Menu Click

--- main.js
+++ main.js
@@ -4,9 +4,22 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight){
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
-    }else {
+    } else {
         navbar.classList.remove('navbar--dark');
     }
 });
+// 마우스를 클릭했을때 원하는 위치로 페이지가 스크롤링 되는 효과
+const navrbarMenu = document.querySelector('.navbar__menu');
+navrbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scroll= document.querySelector(link);
+    scroll.scrollIntoView({behavior:'smooth'});
+}); 
